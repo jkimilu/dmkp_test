ac2292e420b48964c0294071e540cb14f9a40357
Ahmed Maawy <ultimateprogramer@gmail.com>
2014-04-17 15:39:01 +0300

    Critical tree structure changes

--- application/modules/ems/libraries/ems_tree.php
+++ application/modules/ems/libraries/ems_tree.php
@@ -10,57 +10,61 @@ class Ems_Tree
         $ci = &$this->ci;
     }
 
-    public function get_ems_tree()
+    private function build_appendix_response_lead_functions()
     {
-        $response_lead_functions = array(
+        return $response_lead_functions = array(
             "regional_leader",
             "national_director",
             "response_manager",
             "regional_hea_director",
             "partnership_lead"
         );
+    }
 
-        $appendix_sub_item = array(
-            "response_lead_function" => $response_lead_functions,
+    private function build_appendix_sub_item()
+    {
+        return $appendix_sub_item = array(
+            "response_lead_function" => $this->build_appendix_response_lead_functions(),
             "programmes_function",
             "operations_function",
             "support_services_function",
             "liaison_function",
             "security_function",
         );
+    }
 
+    public function get_ems_tree()
+    {
         return array(
-            "intro" => array(
-                "ems_summary" => array(
-                    "summary",
-                    "how_to_use_this_manual",
-                    "why_this_manual",
-                ),
-                "ems_principles" => array(
-                    "management_by_objective",
-                    "unity_of_command",
-                    "flexible_temporary_structure",
-                    "span_of_control",
-                    "common_terminology",
-                ),
-                "ems_functions" => array(
-                    "response_manager",
-                    "programmes",
-                    "operations",
-                    "support_services",
-                    "liaison",
-                    "security",
-                ),
-                "shared_leadership_ems" => array(
-                    "shared_leadership",
-                    "levels_of_accountability_and_responsibility",
-                    "shared_leadership_ems",
-                ),
-                "appendix" => array(
-                    "terms_of_reference" => $appendix_sub_item,
-                    "standard_operating_guidelines" => $appendix_sub_item,
-                )
-            )
+            array("ems_summary", array(
+                "summary",
+                "how_to_use_this_manual",
+                "why_this_manual",
+            )),
+            array("ems_principles", array(
+                "management_by_objective",
+                "unity_of_command",
+                "flexible_temporary_structure",
+                "span_of_control",
+                "common_terminology",
+            )),
+            array("ems_functions", array(
+                "response_manager",
+                "programmes",
+                "operations",
+                "support_services",
+                "liaison",
+                "security",
+            )),
+            array("shared_leadership_ems", array(
+                "shared_leadership",
+                "levels_of_accountability_and_responsibility",
+                "shared_leadership_ems",
+            )),
+            array("appendix", array(
+                array("terms_of_reference", $this->build_appendix_sub_item()),
+                array("standard_operating_guidelines", $this->build_appendix_sub_item()),
+            ))
         );
     }
 }
--- application/modules/ems/views/content/index.php
+++ application/modules/ems/views/content/index.php
@@ -2,19 +2,19 @@
 
 <div class="accordion" id="sections_accordion">
     <div class="accordion-group">
-        <?php foreach($content_tree["intro"] as $tree_item_key => $tree_item_value) : ?>
+        <?php foreach($content_tree as $tree_item_key => $tree_item_value) : ?>
             <div class="accordion-heading">
                 <a class="accordion-toggle" data-toggle="collapse" data-parent="#accordion2" href="#collapse_<?php echo $tree_item_key; ?>">
-                    <strong><?php echo $lang_items[$tree_item_key]; ?></strong>
+                    <strong><?php echo $lang_items[$tree_item_value[0]]; ?></strong>
                 </a>
 
                 <div id="collapse_<?php echo $tree_item_key; ?>" class="accordion-body collapse in">
                     <div class="accordion-inner">
-                        <?php foreach($tree_item_value as $tree_sub_item_key => $tree_sub_item_value) : ?>
-                            <?php if(!is_array($tree_sub_item_value)) : ?>
-                                <div><a href="<?php echo site_url('admin/content/ems/content_edit/'.$tree_item_key.'/'.$tree_sub_item_value); ?>"><?php echo $lang_items[$tree_sub_item_value]; ?></a></div>
+                        <?php foreach($tree_item_value[1] as $tree_sub_item_key => $tree_sub_item_value) : ?>
+                            <?php if(!is_array($tree_sub_item_value[1])) : ?>
+                                <div><a href="<?php echo site_url('admin/content/ems/content_edit/'.$tree_item_value[0].'/'.$tree_sub_item_value); ?>"><?php echo $lang_items[$tree_sub_item_value]; ?></a></div>
                             <?php else : ?>
-                                <div><a href="<?php echo site_url('admin/content/ems/content_edit/'.$tree_item_key.'/'.$tree_sub_item_key); ?>"><?php echo $lang_items[$tree_sub_item_key]; ?></a></div>
+                                <div><a href="<?php echo site_url('admin/content/ems/content_edit/'.$tree_item_value[0].'/'.$tree_sub_item_value[0]); ?>"><?php echo $lang_items[$tree_sub_item_value[0]]; ?></a></div>
                             <?php endif; ?>
                         <?php endforeach; ?>
                     </div>
