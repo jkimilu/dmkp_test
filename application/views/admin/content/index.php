d55a59d91495d57e8dd69c281e6c8d7d0f573903
Ahmed Maawy <ultimateprogramer@gmail.com>
2014-05-06 10:57:37 +0300

    Admin landing changes

--- application/views/admin/content/index.php
+++ application/views/admin/content/index.php
@@ -1,7 +1,35 @@
-<h1>This Page Intentionally Left Blank</h1>
-
-<p>As of Bonfire <?php echo BONFIRE_VERSION ?>, this page has no default content.</p>
-
-<p>This is your playground.</p>
-
-<p>Make it home.</p>
+<ul class="thumbnails">
+    <li class="span4">
+        <div class="thumbnail">
+            <a href="<?php echo site_url('admin/content/ems'); ?>" class="thumbnail">
+                <img style="width:48px;padding:30px;" src="<?php echo base_url('assets/images/paper_content_pencil.png'); ?>" alt="content"/>
+            </a>
+            <div class="caption">
+                <h3>Manage Content</h3>
+                <a class="btn btn-large" href="<?php echo site_url('admin/content/ems'); ?>">Manage</a>
+            </div>
+        </div>
+    </li>
+    <li class="span4">
+        <div class="thumbnail">
+            <a href="<?php echo site_url('admin/settings/users'); ?>" class="thumbnail">
+                <img style="width:48px;padding:30px;" src="<?php echo base_url('assets/images/users.png'); ?>" alt="content"/>
+            </a>
+            <div class="caption">
+                <h3>Manage Users</h3>
+                <a class="btn btn-large" href="<?php echo site_url('admin/settings/users'); ?>">Manage</a>
+            </div>
+        </div>
+    </li>
+    <li class="span4">
+        <div class="thumbnail">
+            <a href="<?php echo site_url('admin/settings/roles'); ?>" class="thumbnail">
+                <img style="width:48px;padding:30px;" src="<?php echo base_url('assets/images/permissions.png'); ?>" alt="content"/>
+            </a>
+            <div class="caption">
+                <h3>Manage Roles and Permissions</h3>
+                <a class="btn btn-large" href="<?php echo site_url('admin/settings/roles'); ?>">Manage</a>
+            </div>
+        </div>
+    </li>
+</ul>
