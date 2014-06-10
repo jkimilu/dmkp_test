91048e2785329263e3c153e4dd8c34146fdd29ac
Ahmed Maawy <ultimateprogramer@gmail.com>
2014-06-10 15:53:40 +0300

    Crutial fix

--- application/modules/ems/views/content/partials/appendices_layout.php
+++ application/modules/ems/views/content/partials/appendices_layout.php
@@ -2,8 +2,10 @@
     <h2>  <?php echo($language[$content_item_key]); ?></h2>
 
     <?php if(isset($content_partials["table"])) : ?>
+
+        <?php echo $content_variables['content']; ?>
+
         <?php foreach($content_partials["table"] as $table_key => $table_value) : ?>
-            <?php echo $content_variables['content']; ?>
             <dl class="dl-horizontal dl-horizontal-big">
                 <dt><?php echo $table_key; ?></dt>
                 <dd><?php echo $table_value; ?></dd>
