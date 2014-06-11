4f128f7a8a425240c5800b48a6e23cfdf1c4e379
Ahmed Maawy <ultimateprogramer@gmail.com>
2014-06-11 15:07:43 +0300

    Fixes

--- application/modules/ems/views/content/partials/appendix_layout.php
+++ application/modules/ems/views/content/partials/appendix_layout.php
@@ -1,153 +1,162 @@
-<?php $tabs = $content_partials["tabs"]; ?>
+<?php $tabs = isset($content_partials["tabs"]) ? $content_partials["tabs"] : null; ?>
 
 <div class="main_content">
-    <h2><i class="fa fa-users"></i> <?php echo($language[$content_item_key]); ?></h2>
 
-    <?php echo $content_variables['content']; ?>
+<?php if($tabs != null) : ?>
 
-    <?php if(count($tabs) > 1) : ?>
-        <ul class="nav nav-tabs sub-functions" id="">
-            <?php $tab_index = 1; ?>
-            <?php foreach($tabs as $tab_key => $tab_content) : ?>
-                <li<?php echo $tab_index == 1 ? ' class="active"' : ''; ?>><a href="#sub_<?php echo $tab_index; ?>" data-toggle="tab" data-original-title="Regional Leader Sub-Function" data-placement="top" class="tipify"><?php echo $tab_key;?></a></li>
-                <?php $tab_index ++ ; ?>
-            <?php endforeach; ?>
-        </ul>
-    <?php endif; ?>
+        <h2><i class="fa fa-users"></i> <?php echo($language[$content_item_key]); ?></h2>
 
-    <?php if(count($tabs) > 1) : ?>
-        <div class="tab-content tab-content-sub-functions">
+        <?php echo $content_variables['content']; ?>
 
-            <?php $tab_index = 1; ?>
+        <?php if(count($tabs) > 1) : ?>
+            <ul class="nav nav-tabs sub-functions" id="">
+                <?php $tab_index = 1; ?>
+                <?php foreach($tabs as $tab_key => $tab_content) : ?>
+                    <li<?php echo $tab_index == 1 ? ' class="active"' : ''; ?>><a href="#sub_<?php echo $tab_index; ?>" data-toggle="tab" data-original-title="Regional Leader Sub-Function" data-placement="top" class="tipify"><?php echo $tab_key;?></a></li>
+                    <?php $tab_index ++ ; ?>
+                <?php endforeach; ?>
+            </ul>
+        <?php endif; ?>
 
-            <?php foreach($tabs as $tab_key => $tab_content) : ?>
-                <div class="tab-pane tor_tab <?php echo $tab_index == 1 ? 'active' : ''; ?>" id="sub_<?php echo $tab_index; ?>">
-                    <h2><?php echo $tab_key; ?></h2>
+        <?php if(count($tabs) > 1) : ?>
+            <div class="tab-content tab-content-sub-functions">
 
-                    <h3>Function Purpose</h3>
+                <?php $tab_index = 1; ?>
 
-                    <?php echo $tab_content['content_purpose']; ?>
+                <?php foreach($tabs as $tab_key => $tab_content) : ?>
+                    <div class="tab-pane tor_tab <?php echo $tab_index == 1 ? 'active' : ''; ?>" id="sub_<?php echo $tab_index; ?>">
+                        <h2><?php echo $tab_key; ?></h2>
 
-                    <h3>Specific Role</h3>
+                        <h3>Function Purpose</h3>
 
-                    <?php echo $tab_content['content_role']; ?>
+                        <?php echo $tab_content['content_purpose']; ?>
 
-                    <ul class="nav nav-tabs">
-                        <li class="active"><a href="#tor_<?php echo $tab_index; ?>" data-toggle="tab"><i class="fa fa-list-ul"></i> Terms of Reference</a></li>
-                        <li><a href="#sog_<?php echo $tab_index; ?>" data-toggle="tab"><i class="fa fa-list-ul"></i> Standard Operating Guidelines</a></li>
-                    </ul>
+                        <h3>Specific Role</h3>
 
-                    <div class="guidance">
-                        <div class="alert alert-block alert-danger fade in">
-                            <button type="button" class="close" data-dismiss="alert">×</button>
-                            <h5><i class="fa fa-exclamation-triangle"></i> Guidance</h5>
-                            <ul>
-                                <li>Achievement of TOR objectives to the quality criteria is mandatory.</li>
-                                <li>SOGs are optional and should adapted to ensure that the TOR objective is achieved to quality criteria</li>
-                                <li>All Tools &amp; Standards referenced in the TOR/SOGs can be found in the "Operational Guidance" framework, which is available on WV Central</li>
-                            </ul>
+                        <?php echo $tab_content['content_role']; ?>
+
+                        <ul class="nav nav-tabs">
+                            <li class="active"><a href="#tor_<?php echo $tab_index; ?>" data-toggle="tab"><i class="fa fa-list-ul"></i> Terms of Reference</a></li>
+                            <li><a href="#sog_<?php echo $tab_index; ?>" data-toggle="tab"><i class="fa fa-list-ul"></i> Standard Operating Guidelines</a></li>
+                        </ul>
+
+                        <div class="guidance">
+                            <div class="alert alert-block alert-danger fade in">
+                                <button type="button" class="close" data-dismiss="alert">×</button>
+                                <h5><i class="fa fa-exclamation-triangle"></i> Guidance</h5>
+                                <ul>
+                                    <li>Achievement of TOR objectives to the quality criteria is mandatory.</li>
+                                    <li>SOGs are optional and should adapted to ensure that the TOR objective is achieved to quality criteria</li>
+                                    <li>All Tools &amp; Standards referenced in the TOR/SOGs can be found in the "Operational Guidance" framework, which is available on WV Central</li>
+                                </ul>
+                            </div>
+                        </div>
+
+                        <div class="tab-content">
+                            <div id="tor_<?php echo $tab_index; ?>" class="tab-pane active">
+
+                                <div class="well">
+                                    <h4><?php echo $tab_content["title"]; ?></h4>
+                                    <?php echo $tab_content["tor"]; ?>
+                                </div>
+                            </div>
+
+                            <div id="sog_<?php echo $tab_index; ?>" class="tab-pane">
+                                <div class="key alert alert-block alert-danger fade in">
+                                    <button type="button" class="close" data-dismiss="alert">×</button>
+                                    <h5><i class="fa fa-key"></i> Key</h5>
+
+                                    <dl class="dl-horizontal">
+                                        <dt><span class="badge badge-important">m</span></dt>
+                                        <dd>Mandatory quality criteria to be achieved to successfully complete the TOR objective</dd>
+                                    </dl>
+
+                                    <dl class="dl-horizontal">
+                                        <dt><span class="badge badge-warning">e</span></dt>
+                                        <dd>External sourced Tool or Standard to World Vision</dd>
+                                    </dl>
+
+                                    <dl class="dl-horizontal">
+                                        <dt><span class="badge">i</span></dt>
+                                        <dd>Internal sourced Tool or Standard for World Vision</dd>
+                                    </dl>
+
+                                    <?php echo $tab_content["sog"]; ?>
+                                </div>
+                            </div>
                         </div>
                     </div>
+                    <?php $tab_index ++; ?>
+                <?php endforeach; ?>
+            </div>
 
-                    <div class="tab-content">
-                        <div id="tor_<?php echo $tab_index; ?>" class="tab-pane active">
+        <?php else : ?>
 
-                            <div class="well">
-                                <h4><?php echo $tab_content["title"]; ?></h4>
-                                <?php echo $tab_content["tor"]; ?>
-                            </div>
-                        </div>
+            <h3>Function Purpose</h3>
 
-                        <div id="sog_<?php echo $tab_index; ?>" class="tab-pane">
-                            <div class="key alert alert-block alert-danger fade in">
-                                <button type="button" class="close" data-dismiss="alert">×</button>
-                                <h5><i class="fa fa-key"></i> Key</h5>
+            <?php echo $tabs[0]['content_purpose']; ?>
 
-                                <dl class="dl-horizontal">
-                                    <dt><span class="badge badge-important">m</span></dt>
-                                    <dd>Mandatory quality criteria to be achieved to successfully complete the TOR objective</dd>
-                                </dl>
+            <h3>Specific Role</h3>
 
-                                <dl class="dl-horizontal">
-                                    <dt><span class="badge badge-warning">e</span></dt>
-                                    <dd>External sourced Tool or Standard to World Vision</dd>
-                                </dl>
+            <?php echo $tabs[0]['content_role']; ?>
 
-                                <dl class="dl-horizontal">
-                                    <dt><span class="badge">i</span></dt>
-                                    <dd>Internal sourced Tool or Standard for World Vision</dd>
-                                </dl>
+            <ul class="nav nav-tabs">
+                <li class="active"><a href="#tor" data-toggle="tab"><i class="fa fa-list-ul"></i> Terms of Reference</a></li>
+                <li><a href="#sog" data-toggle="tab"><i class="fa fa-list-ul"></i> Standard Operating Guidelines</a></li>
+            </ul>
 
-                                <?php echo $tab_content["sog"]; ?>
-                            </div>
-                        </div>
+            <div class="guidance">
+                <div class="alert alert-block alert-danger fade in">
+                    <button type="button" class="close" data-dismiss="alert">×</button>
+                    <h5><i class="fa fa-exclamation-triangle"></i> Guidance</h5>
+                    <ul>
+                        <li>Achievement of TOR objectives to the quality criteria is mandatory.</li>
+                        <li>SOGs are optional and should adapted to ensure that the TOR objective is achieved to quality criteria</li>
+                        <li>All Tools &amp; Standards referenced in the TOR/SOGs can be found in the "Operational Guidance" framework, which is available on WV Central</li>
+                    </ul>
+                </div>
+            </div>
+
+            <div class="tab-content">
+                <div id="tor" class="tab-pane active">
+
+                    <div class="well">
+                        <h4><?php echo $tabs[0]["title"]; ?></h4>
+                        <?php echo $tabs[0]["tor"]; ?>
                     </div>
                 </div>
-                <?php $tab_index ++; ?>
-            <?php endforeach; ?>
-        </div>
 
+                <div id="sog" class="tab-pane">
+                    <div class="key alert alert-block alert-danger fade in">
+                        <button type="button" class="close" data-dismiss="alert">×</button>
+                        <h5><i class="fa fa-key"></i> Key</h5>
+
+                        <dl class="dl-horizontal">
+                            <dt><span class="badge badge-important">m</span></dt>
+                            <dd>Mandatory quality criteria to be achieved to successfully complete the TOR objective</dd>
+                        </dl>
+
+                        <dl class="dl-horizontal">
+                            <dt><span class="badge badge-warning">e</span></dt>
+                            <dd>External sourced Tool or Standard to World Vision</dd>
+                        </dl>
+
+                        <dl class="dl-horizontal">
+                            <dt><span class="badge">i</span></dt>
+                            <dd>Internal sourced Tool or Standard for World Vision</dd>
+                        </dl>
+                    </div>
+
+                    <?php echo $tabs[0]["sog"]; ?>
+
+                </div>
+            </div>
+
+        <?php endif; ?>
     <?php else : ?>
 
-        <h3>Function Purpose</h3>
-
-        <?php echo $tabs[0]['content_purpose']; ?>
-
-        <h3>Specific Role</h3>
-
-        <?php echo $tabs[0]['content_role']; ?>
-
-        <ul class="nav nav-tabs">
-            <li class="active"><a href="#tor" data-toggle="tab"><i class="fa fa-list-ul"></i> Terms of Reference</a></li>
-            <li><a href="#sog" data-toggle="tab"><i class="fa fa-list-ul"></i> Standard Operating Guidelines</a></li>
-        </ul>
-
-        <div class="guidance">
-            <div class="alert alert-block alert-danger fade in">
-                <button type="button" class="close" data-dismiss="alert">×</button>
-                <h5><i class="fa fa-exclamation-triangle"></i> Guidance</h5>
-                <ul>
-                    <li>Achievement of TOR objectives to the quality criteria is mandatory.</li>
-                    <li>SOGs are optional and should adapted to ensure that the TOR objective is achieved to quality criteria</li>
-                    <li>All Tools &amp; Standards referenced in the TOR/SOGs can be found in the "Operational Guidance" framework, which is available on WV Central</li>
-                </ul>
-            </div>
-        </div>
-
-        <div class="tab-content">
-            <div id="tor" class="tab-pane active">
-
-                <div class="well">
-                    <h4><?php echo $tabs[0]["title"]; ?></h4>
-                    <?php echo $tabs[0]["tor"]; ?>
-                </div>
-            </div>
-
-            <div id="sog" class="tab-pane">
-                <div class="key alert alert-block alert-danger fade in">
-                    <button type="button" class="close" data-dismiss="alert">×</button>
-                    <h5><i class="fa fa-key"></i> Key</h5>
-
-                    <dl class="dl-horizontal">
-                        <dt><span class="badge badge-important">m</span></dt>
-                        <dd>Mandatory quality criteria to be achieved to successfully complete the TOR objective</dd>
-                    </dl>
-
-                    <dl class="dl-horizontal">
-                        <dt><span class="badge badge-warning">e</span></dt>
-                        <dd>External sourced Tool or Standard to World Vision</dd>
-                    </dl>
-
-                    <dl class="dl-horizontal">
-                        <dt><span class="badge">i</span></dt>
-                        <dd>Internal sourced Tool or Standard for World Vision</dd>
-                    </dl>
-                </div>
-
-                <?php echo $tabs[0]["sog"]; ?>
-
-            </div>
-        </div>
+        <h2>  <?php echo($language[$content_item_key]); ?></h2>
+        <?php echo $content_variables['content']; ?>
 
     <?php endif; ?>
 </div>
--- application/modules/ems/views/content/partials/ems_functions_layout.php
+++ application/modules/ems/views/content/partials/ems_functions_layout.php
@@ -1,11 +1,20 @@
 <div class="main_content">
-    <h2>  <i class="<?php echo $content_partials['icon']; ?>"></i> "<?php echo $content_partials['pre_append']; ?>" - <?php echo($language[$content_item_key]); ?></h2>
-    <?php echo $content_variables['content']; ?>
+    <?php if(isset($content_partials["tor_sog_link"])) : ?>
 
-    <hr/>
+        <h2>  <i class="<?php echo $content_partials['icon']; ?>"></i> "<?php echo $content_partials['pre_append']; ?>" - <?php echo($language[$content_item_key]); ?></h2>
+        <?php echo $content_variables['content']; ?>
 
-    <div class="row-fluid tor_sog">
-        <a class="btn span6" href="<?php echo $content_partials["tor_sog_link"];?>">Terms Of Reference</a>
-        <a class="btn span6" href="<?php echo $content_partials["tor_sog_link"];?>">Standard Operating Guidelines</a>
-    </div>
+        <hr/>
+
+        <div class="row-fluid tor_sog">
+            <a class="btn span6" href="<?php echo $content_partials["tor_sog_link"];?>">Terms Of Reference</a>
+            <a class="btn span6" href="<?php echo $content_partials["tor_sog_link"];?>">Standard Operating Guidelines</a>
+        </div>
+
+    <?php else : ?>
+
+        <h2>  <?php echo($language[$content_item_key]); ?></h2>
+        <?php echo $content_variables['content']; ?>
+
+    <?php endif; ?>
 </div>
