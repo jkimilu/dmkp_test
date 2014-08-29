ef2b32a7cef271d86bbedcd340d8bfc88fde1964
ultimateprogramer <ultimateprogramer@gmail.com>
2014-08-29 22:54:29 +0300

    PDF Enhancement

--- application/modules/ems/views/content/partials/pdf_toc.php
+++ application/modules/ems/views/content/partials/pdf_toc.php
@@ -3,6 +3,11 @@
 
 <?php foreach($ems_tree as $pdf_tree_item) : ?>
     <p><?php echo $language[$pdf_tree_item[0]];?></p>
+    <ul>
+        <?php foreach($pdf_tree_item[1] as $pdf_sub_tree_item): ?>
+            <li><?php echo $language[$pdf_sub_tree_item];?></li>
+        <?php endforeach; ?>
+    </ul>
 <?php endforeach; ?>
 
 <pagebreak />
