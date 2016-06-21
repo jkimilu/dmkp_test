2fccd7867227309728c9d9ec15a858c218cc2d69
Ahmed Maawy <ultimateprogramer@gmail.com>
2016-06-21 17:34:45 +0300

    Create landing views for DM Policies, DM Preparedness and Capacity Building

---
+++ application/modules/capacity_building/views/index.php
@@ -0,0 +1,127 @@
+<!-- .row-alert -->
+<div class="row-fluid row-alert">
+    <div class="span12">
+        <!-- .alert -->
+        <div class="alert alert-block alert-info fade in">
+            <button type="button" class="close" data-dismiss="alert">×</button>
+            <p><i class="fa fa-warning"></i> Regular alerts will go here. If any.</p>
+        </div>
+        <!-- /.alert -->
+    </div>
+</div>
+<!-- /.row-alert -->
+
+<!-- row-fluid -->
+<div class="row-fluid row-fluid-into">
+    <!-- left -->
+    <div class="span10">
+        <div class="dm_intro">
+            <div class="img" style="background-image: url('<?php echo(base_url('themes/dmkp/img/capacity_building.jpg')); ?>');">&nbsp;</div>
+            <h2><i class="fa fa-graduation-cap"></i> Capacity Building</h2>
+            <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat. Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat, vel illum dolore eu feugiat nulla facilisis at vero eros et accumsan et iusto odio dignissim qui blandit praesent luptatum zzril delenit augue duis dolore te feugait nulla facilisi. Nam liber tempor cum soluta nobis eleifend option congue nihil imperdiet doming id quod mazim placerat facer possim assum. Typi non habent claritatem insitam; est usus legentis in iis qui facit eorum claritatem.</p>
+        </div>
+    </div>
+    <!-- /left -->
+
+    <!-- right -->
+    <div class="span2">
+        <div class="pane pane_insights">
+            <h5><i class="fa fa-eye"></i> Key Insights</h5>
+            <ul class="unstyled">
+                <li><i class="fa fa-file"></i> 230 files</li>
+                <li><i class="fa fa-file"></i> 42 links</li>
+            </ul>
+        </div>
+    </div>
+    <!-- /right -->
+</div>
+<!-- /row-fluid -->
+<!-- row-fluid -->
+
+<div class="row-fluid">
+    <!-- .overflow_auto_mru -->
+    <div class="overflow_auto overflow_auto_mru">
+        <ul class="nav nav-tabs ul_mru pull-left">
+            <li class="active"><a href="#none"><h4>eCampus Courses</h4></a></li>
+            <li><a href="capacity_building_other.html"><h4>Other Resourses</h4></a></li>
+        </ul>
+        <!-- search form -->
+        <form class="form-search form-search-mru pull-right" enctype="multipart/form-data" method="get" action="">
+            <input name="search" type="text" placeholder="Search" class="search-query">
+        </form>
+        <!-- /search form -->
+    </div>
+    <!-- /.overflow_auto_mru -->
+
+    <!-- mru -->
+    <div class="mru">
+        <!-- .mru_tbl -->
+        <table class="table table-condensed table-striped table-hover mru_tbl">
+            <thead>
+                <tr>
+                    <th class="th_grouping">Grouping</th>
+                    <th class="th_guidance">Guidance Descriptors</th>
+                    <th class="th_links">Links &amp; Resources</th>
+                    <th class="th_person">Key Contact Person</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td>High Level / Strategic Guidance</td>
+                    <td>
+                        <h5>Disaster Management Funding Policy</h5>
+                        <p class="desc">This policy will provide further guidance around the areas referred to in the Partnership Policy on Relief Funding (refer #1 above). The draft management policy has been prepared in recent months, and is anticipated to be finalised in early 2013.</p>
+                    </td>
+                    <td>
+                        <p>An abbreviated version is available in the Humanitarian Pocket Guide. For the full version please follow:</p>
+                        <a href="#none"><i class="fa fa-file"></i> Disaster Management Policy</a>
+                    </td>
+                    <td><a href="#none">Isabel Gomes</a></td>
+                </tr>
+                <!-- tr -->
+
+                <!-- tr -->
+                <tr>
+                    <td>Key Policies</td>
+                    <td>
+                        <h5>Donor Funding Guidelines</h5>
+                        <p class="desc">Each grant comes with specific terms and conditions that must be met.</p>
+                    </td>
+                    <td><a href="#none"><i class="fa fa-file"></i> Core Security Requirements</a></td>
+                    <td><a href="#none">Ian Ridley</a></td>
+                </tr>
+                <!-- tr -->
+            </tbody>
+        </table>
+        <!-- .mru_tbl -->
+
+        <div class="pagination">
+            <ul>
+                <li class="disabled"><a href="#">« Prev</a></li>
+                <li class="active"><a href="#">1</a></li>
+                <li><a href="#">2</a></li>
+                <li><a href="#">3</a></li>
+                <li><a href="#">4</a></li>
+                <li><a href="#">5</a></li>
+                <li><a href="#">Next »</a></li>
+            </ul>
+        </div>
+    </div>
+    <!-- mru -->
+
+</div>
+<!-- /row-fluid -->
+
+<!-- .row-fluid footer -->
+<div class="row-fluid footer">
+    <hr>
+    <ul class="unstyled inline pull-left">
+        <li><a href="copyright.html">Copyright Notice</a></li>
+        <li class="administrator"><a href="admin.html" target="_blank">Admin</a></li>
+    </ul>
+
+    <ul class="unstyled inline pull-right">
+        <li>© World Vision International 2016 | All Rights Reserved</li>
+    </ul>
+</div>
+<!-- /.row-fluid footer -->
---
+++ application/modules/dm_preparedness/views/index.php
@@ -0,0 +1,211 @@
+<!-- .row-alert -->
+<div class="row-fluid row-alert">
+    <div class="span12">
+        <!-- .alert -->
+        <div class="alert alert-block alert-info fade in">
+            <button type="button" class="close" data-dismiss="alert">×</button>
+            <p><i class="fa fa-warning"></i> Regular alerts will go here. If any.</p>
+        </div>
+        <!-- /.alert -->
+    </div>
+</div>
+<!-- /.row-alert -->
+
+<!-- row-fluid -->
+<div class="row-fluid row-fluid-into">
+    <!-- left -->
+    <div class="span10">
+
+        <div class="dm_intro">
+            <div class="img" style="background-image: url('<?php echo(base_url('themes/dmkp/img/hurricane.jpg')); ?>');">&nbsp;</div>
+
+            <h2><i class="fa fa-umbrella"></i> Disaster Management Preparedness</h2>
+
+            <p>World Vision defines preparedness as <span style="font-style:italic">"Achieving preparedness means organisational and community readiness systems, procedures and resources are available to provide timely assistance to those affected. Standards, tools, mechanisms and resources are in place. Disaster-management plans are in place which enable communities to identify areas of vulnerability to their immediate and long-term well-being; to mitigate risks; and to prepare for response in the case of risks which cannot be resolved."</span></p>
+
+            <p>This section contains helpful resources to help achieve preparedness in the follow categories.</p>
+        </div>
+    </div>
+    <!-- /left -->
+
+    <!-- right -->
+    <div class="span2">
+        <div class="pane pane_insights">
+            <h5><i class="fa fa-eye"></i> Key Insights</h5>
+
+            <ul class="unstyled">
+                <li><i class="fa fa-file"></i> 230 files</li>
+                <li><i class="fa fa-file"></i> 42 links</li>
+            </ul>
+
+        </div>
+    </div>
+    <!-- /right -->
+
+</div>
+<!-- /row-fluid -->
+<!-- row-fluid -->
+<div class="row-fluid">
+    <!-- .overflow_auto_mru -->
+    <div class="overflow_auto overflow_auto_mru">
+        <ul class="nav nav-tabs ul_mru pull-left">
+            <li class="active"><a href="#none"><h4>Tools &amp; Templates</h4></a></li>
+            <li><a href="dm_prepare_recommended.html"><h4>Recommended</h4></a></li>
+            <li><a href="dm_prepare_useful.html"><h4>Useful</h4></a></li>
+        </ul>
+
+        <!-- search form -->
+        <form class="form-search form-search-mru pull-right" enctype="multipart/form-data" method="get" action="">
+            <input name="search" type="text" placeholder="Search" class="search-query">
+        </form>
+        <!-- /search form -->
+    </div>
+    <!-- /.overflow_auto_mru -->
+
+    <!-- mru -->
+    <div class="mru">
+        <!-- .mru_tbl -->
+        <table class="table table-condensed table-striped table-hover mru_tbl">
+            <thead>
+                <tr>
+                    <th class="th_grouping">Grouping</th>
+                    <th class="th_guidance">Guidance Descriptors</th>
+                    <th class="th_links">Links &amp; Resources</th>
+                    <th class="th_version">Latest Version</th>
+                    <th class="th_gate">Gate Keeper</th>
+                </tr>
+            </thead>
+
+            <tbody>
+                <!-- tr -->
+                <tr>
+                    <td>Cross-cutting Principles and Codes of Conduct</td>
+                    <td>
+                        <h5>Overview of Changes to National Office Disaster Preparedness Process and Documents</h5>
+                        <p class="desc">This manual is designed to provide managers the knowledge required to safely implement programs, through a broad understanding of World Vision's policies, protocols, and tools for safe program implementation.</p>
+                    </td>
+                    <td>
+                        <a href="#none"><i class="fa fa-file"></i> Disaster Management Standards</a>
+                        <a href="#none"><i class="fa fa-file"></i> EMS Manual</a>
+                    </td>
+                    <td>May 2010</td>
+                    <td><a href="#none">Christine Ash Beuchner</a></td>
+                </tr>
+                <!-- tr -->
+
+                <!-- tr -->
+                <tr>
+                    <td>High Level / Strategic Guidance</td>
+                    <td>
+                        <h5>Principles of Conduct for the International Red Cross and Red Crescent Movement and NGOs in Disaster Response Programmes</h5>
+                        <p class="desc">A voluntary code that lays down ten points of principle which WV has agreed to adhere to in our disaster response work</p>
+                    </td>
+                    <td><a href="#none"><i class="fa fa-file"></i> Humanitarian Pocket Guide-Code of Conduct</a></td>
+                    <td>February 2010</td>
+                    <td><a href="#none">Laurence Baird</a></td>
+                </tr>
+                <!-- tr -->
+
+                <!-- tr -->
+                <tr>
+                    <td>Cross-cutting Principles and Codes of Conduct</td>
+                    <td>
+                        <h5>Humanitarian Accountability Partnership Principles </h5>
+                        <p class="desc">A set of principles to make humanitarian action more accountable to beneficiaries</p>
+                    </td>
+                    <td><a href="#none"><i class="fa fa-file"></i> EMS Manual</a></td>
+                    <td>March 2010</td>
+                    <td><a href="#none">Isabel Gomes</a></td>
+                </tr>
+                <!-- tr -->
+
+                <!-- tr -->
+                <tr>
+                    <td>Key Policies</td>
+                    <td>
+                        <h5>Core Security Requirements</h5>
+                        <p class="desc">Consists of a minimum of security requirements developed to suit the varied operational needs and environments in which WV has staff, assets, and programmes. </p>
+                    </td>
+                    <td>
+                        <p>National Director/ROSM</p>
+                    </td>
+                    <td>December 2010</td>
+                    <td><a href="#none">Jamo Huddle</a></td>
+                </tr>
+                <!-- tr -->
+
+                <!-- tr -->
+                <tr>
+                    <td>Key Policies</td>
+                    <td>
+                        <h5>Partnership Board Policy on Relief Funding</h5>
+                        <p class="desc">The purpose of this policy is to ensure that appropriate funding mechanisms and liquidity exist to facilitate World Vision's emergency responses. This is a high level policy which outlines various mechanisms available to ensure resources are available on a timely basis and to respond to the changing needs in our emergencies. The policy was approved by the WVI Board in May 2012.</p>
+                    </td>
+                    <td><a href="#none"><i class="fa fa-file"></i> Disaster Management Policy</a></td>
+                    <td>June 2010</td>
+                    <td><a href="#none">Isabel Gomes</a></td>
+                </tr>
+                <!-- tr -->
+
+                <!-- tr -->
+                <tr>
+                    <td>High Level / Strategic Guidance</td>
+                    <td>
+                        <h5>Disaster Management Funding Policy</h5>
+                        <p class="desc">This policy will provide further guidance around the areas referred to in the Partnership Policy on Relief Funding (refer #1 above). The draft management policy has been prepared in recent months, and is anticipated to be finalised in early 2013.</p>
+                    </td>
+                    <td>
+                        <p>An abbreviated version is available in the Humanitarian Pocket Guide. For the full version please follow:</p>
+                        <a href="#none"><i class="fa fa-file"></i> Disaster Management Policy</a>
+                    </td>
+                    <td>July 2010</td>
+                    <td><a href="#none">Isabel Gomes</a></td>
+                </tr>
+                <!-- tr -->
+
+                <!-- tr -->
+                <tr>
+                    <td>Key Policies</td>
+                    <td>
+                        <h5>Donor Funding Guidelines</h5>
+                        <p class="desc">Each grant comes with specific terms and conditions that must be met.</p>
+                    </td>
+                    <td><a href="#none"><i class="fa fa-file"></i> Core Security Requirements</a></td>
+                    <td>Depends on National Office</td>
+                    <td><a href="#none">Ian Ridley</a></td>
+                </tr>
+            <!-- tr -->
+            </tbody>
+        </table>
+        <!-- .mru_tbl -->
+
+        <div class="pagination">
+            <ul>
+                <li class="disabled"><a href="#">« Prev</a></li>
+                <li class="active"><a href="#">1</a></li>
+                <li><a href="#">2</a></li>
+                <li><a href="#">3</a></li>
+                <li><a href="#">4</a></li>
+                <li><a href="#">5</a></li>
+                <li><a href="#">Next »</a></li>
+            </ul>
+        </div>
+    </div>
+    <!-- mru -->
+</div>
+<!-- /row-fluid -->
+
+<!-- .row-fluid footer -->
+<div class="row-fluid footer">
+    <hr>
+
+    <ul class="unstyled inline pull-left">
+        <li><a href="copyright.html">Copyright Notice</a></li>
+        <li class="administrator"><a href="admin.html" target="_blank">Admin</a></li>
+    </ul>
+
+    <ul class="unstyled inline pull-right">
+        <li>© World Vision International 2016 | All Rights Reserved</li>
+    </ul>
+</div>
+<!-- /.row-fluid footer -->
