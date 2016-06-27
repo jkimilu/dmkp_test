8ee1163ad93bbd1abba52f531b9d3373cc33b097
Ahmed Maawy <ultimateprogramer@gmail.com>
2016-06-27 09:13:19 +0300

    Revamp admin dashboard to point to various content sections

--- application/views/admin/content/index.php
+++ application/views/admin/content/index.php
@@ -1,35 +1,99 @@
-<ul class="thumbnails">
-    <li class="span4">
-        <div class="thumbnail">
-            <a href="<?php echo site_url('admin/content/ems'); ?>" class="thumbnail">
-                <img style="width:48px;padding:30px;" src="<?php echo base_url('assets/images/paper_content_pencil.png'); ?>" alt="content"/>
-            </a>
-            <div class="caption">
-                <h3>Manage Content</h3>
-                <a class="btn btn-large" href="<?php echo site_url('admin/content/ems'); ?>">Manage</a>
+<h3>Content</h3>
+
+<div class="row">
+    <ul class="thumbnails">
+        <li class="span4">
+            <div class="thumbnail">
+                <a href="<?php echo site_url('admin/content/capacity_building'); ?>" class="thumbnail">
+                    <i class="fa fa-graduation-cap" style="width:100%;padding:30px;font-size:64px;text-align:center;"></i>
+                </a>
+                <div class="caption">
+                    <h3>Manage Capacity Building Content</h3>
+                    <a class="btn btn-large" href="<?php echo site_url('admin/content/capacity_building'); ?>">Manage</a>
+                </div>
             </div>
-        </div>
-    </li>
-    <li class="span4">
-        <div class="thumbnail">
-            <a href="<?php echo site_url('admin/settings/users'); ?>" class="thumbnail">
-                <img style="width:48px;padding:30px;" src="<?php echo base_url('assets/images/users.png'); ?>" alt="content"/>
-            </a>
-            <div class="caption">
-                <h3>Manage Users</h3>
-                <a class="btn btn-large" href="<?php echo site_url('admin/settings/users'); ?>">Manage</a>
+        </li>
+
+        <li class="span4">
+            <div class="thumbnail">
+                <a href="<?php echo site_url('admin/content/dm_policies'); ?>" class="thumbnail">
+                    <i class="fa fa-newspaper-o" style="width:100%;padding:30px;font-size:64px;text-align:center;"></i>
+                </a>
+                <div class="caption">
+                    <h3>Manage DM Policies Content</h3>
+                    <a class="btn btn-large" href="<?php echo site_url('admin/content/dm_policies'); ?>">Manage</a>
+                </div>
             </div>
-        </div>
-    </li>
-    <li class="span4">
-        <div class="thumbnail">
-            <a href="<?php echo site_url('admin/settings/roles'); ?>" class="thumbnail">
-                <img style="width:48px;padding:30px;" src="<?php echo base_url('assets/images/permissions.png'); ?>" alt="content"/>
-            </a>
-            <div class="caption">
-                <h3>Manage Roles and Permissions</h3>
-                <a class="btn btn-large" href="<?php echo site_url('admin/settings/roles'); ?>">Manage</a>
+        </li>
+
+        <li class="span4">
+            <div class="thumbnail">
+                <a href="<?php echo site_url('admin/content/dm_preparedness'); ?>" class="thumbnail">
+                    <i class="fa fa-umbrella" style="width:100%;padding:30px;font-size:64px;text-align:center;"></i>
+                </a>
+                <div class="caption">
+                    <h3>Manage DM Preparedness Content</h3>
+                    <a class="btn btn-large" href="<?php echo site_url('admin/content/dm_preparedness'); ?>">Manage</a>
+                </div>
             </div>
-        </div>
-    </li>
-</ul>
+        </li>
+    </ul>
+</div>
+
+<div class="row">
+    <ul class="thumbnails">
+        <li class="span4">
+            <div class="thumbnail">
+                <a href="<?php echo site_url('admin/content/dm_standards'); ?>" class="thumbnail">
+                    <i class="fa fa-list-ul" style="width:100%;padding:30px;font-size:64px;text-align:center;"></i>
+                </a>
+                <div class="caption">
+                    <h3>Manage DM Standards Content</h3>
+                    <a class="btn btn-large" href="<?php echo site_url('admin/content/dm_standards'); ?>">Manage</a>
+                </div>
+            </div>
+        </li>
+
+        <li class="span4">
+            <div class="thumbnail">
+                <a href="<?php echo site_url('admin/content/ems'); ?>" class="thumbnail">
+                    <i class="fa fa-book" style="width:100%;padding:30px;font-size:64px;text-align:center;"></i>
+                </a>
+                <div class="caption">
+                    <h3>Manage EMS Content</h3>
+                    <a class="btn btn-large" href="<?php echo site_url('admin/content/ems'); ?>">Manage</a>
+                </div>
+            </div>
+        </li>
+    </ul>
+</div>
+
+<h3>Administration</h3>
+
+<div class="row">
+    <ul class="thumbnails">
+        <li class="span4">
+            <div class="thumbnail">
+                <a href="<?php echo site_url('admin/settings/users'); ?>" class="thumbnail">
+                    <img style="width:48px;padding:30px;" src="<?php echo base_url('assets/images/users.png'); ?>" alt="content"/>
+                </a>
+                <div class="caption">
+                    <h3>Manage Users</h3>
+                    <a class="btn btn-large" href="<?php echo site_url('admin/settings/users'); ?>">Manage</a>
+                </div>
+            </div>
+        </li>
+
+        <li class="span4">
+            <div class="thumbnail">
+                <a href="<?php echo site_url('admin/settings/roles'); ?>" class="thumbnail">
+                    <img style="width:48px;padding:30px;" src="<?php echo base_url('assets/images/permissions.png'); ?>" alt="content"/>
+                </a>
+                <div class="caption">
+                    <h3>Manage Roles and Permissions</h3>
+                    <a class="btn btn-large" href="<?php echo site_url('admin/settings/roles'); ?>">Manage</a>
+                </div>
+            </div>
+        </li>
+    </ul>
+</div>
