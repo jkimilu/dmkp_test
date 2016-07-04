d5561eb65de11dbf8f0ac32b2add73d408a56b06
Ahmed Maawy <ultimateprogramer@gmail.com>
2016-07-04 13:36:58 +0300

    Implement ID filters for Capacity Building and DM Preparedness

--- application/modules/capacity_building/controllers/capacity_building.php
+++ application/modules/capacity_building/controllers/capacity_building.php
@@ -56,20 +56,46 @@ class capacity_building extends BaseResourceController
         $categoryKeys = array_keys($categories);
         $category = $this->input->get('category', null);
 
+		// Specific for search
+		$id = $this->input->get('id', 0);
+		$activeCategory = null;
+
+		if($id > 0) {
+			$contentItem = $this->Content_Model->find($id);
+
+			if($contentItem) {
+				$activeCategory = $contentItem->category;
+			}
+		}
+
         if($category == null) {
             $category = $categoryKeys[0];
         }
 
-        foreach($categoryKeys as $key) {
-            if($key == $category) {
-                Template::set($key.'_active', true);
-            } else {
+        if($id > 0) {
+            foreach($categoryKeys as $key) {
                 Template::set($key.'_active', false);
+            }
+
+            Template::set($activeCategory.'_active', true);
+        } else {
+            foreach($categoryKeys as $key) {
+                if($key == $category) {
+                    Template::set($key.'_active', true);
+                } else {
+                    Template::set($key.'_active', false);
+                }
             }
         }
 
         Template::set('keyInsights', $this->showKeyInsights($this->resourceCategory));
-		Template::set('listView', $this->showResourcesList($this->resourceCategory, $this->Content_Model, true, $category, 'table table-condensed table-striped table-hover mru_tbl'));
+		Template::set('listView', $this->showResourcesList($this->resourceCategory,
+            $this->Content_Model,
+            true,
+            $category,
+            'table table-condensed table-striped table-hover mru_tbl',
+            $id
+        ));
         Template::set('categories', $categories);
         Template::set('tabsUrl', site_url('capacity_building'));
 		Template::render();
