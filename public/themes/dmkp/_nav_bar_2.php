e85f52569604804748b864e37ddeea14c9e6e265
Ahmed Maawy <ultimateprogramer@gmail.com>
2016-07-18 09:54:00 +0300

    Temporary: Make DM Standards only visible to admins

--- public/themes/dmkp/_nav_bar_2.php
+++ public/themes/dmkp/_nav_bar_2.php
@@ -3,7 +3,13 @@
     <div class="max-width">
         <ul class="unstyled inline ul_navbar2">
             <li <?php echo(isset($dm_policies_active) ? 'class="active"' : ''); ?>><a data-original-title="Disaster Management Policies" href="<?php echo site_url('dm_policies'); ?>" class="tipify" title="" data-placement="bottom"><i class="fa fa-newspaper-o"></i> DM Policies</a></li>
-            <li <?php echo(isset($dm_standards_active) ? 'class="active"' : ''); ?>><a data-original-title="Disaster Management Standards" href="<?php echo site_url('dm_standards'); ?>" class="tipify" title="" data-placement="bottom"><i class="fa fa-list-ul"></i> DM Standards</a></li>
+
+            <!-- TODO: Temporarily only visible to admins: Remove constraint after approval -->
+
+            <?php if($is_admin) : ?>
+                <li <?php echo(isset($dm_standards_active) ? 'class="active"' : ''); ?>><a data-original-title="Disaster Management Standards" href="<?php echo site_url('dm_standards'); ?>" class="tipify" title="" data-placement="bottom"><i class="fa fa-list-ul"></i> DM Standards</a></li>
+            <?php endif; ?>
+
             <li <?php echo(isset($ems_active) ? 'class="active"' : ''); ?>><a data-original-title="Emergence Management Manual" href="<?php echo site_url('ems'); ?>" class="tipify" title="" data-placement="bottom"><i class="fa fa-book"></i> EMS Manual</a></li>
             <li <?php echo(isset($dm_preparedness_active) ? 'class="active"' : ''); ?>><a data-original-title="Disaster Management Preparedness" href="<?php echo site_url('dm_preparedness'); ?>" class="tipify" title="" data-placement="bottom"><i class="fa fa-umbrella"></i> DM Preparedness</a></li>
             <li <?php echo(isset($capacity_building_active) ? 'class="active"' : ''); ?>><a data-original-title="Capacity Building" href="<?php echo site_url('capacity_building'); ?>" class="tipify" title="" data-placement="bottom"><i class="fa fa-graduation-cap"></i> Capacity Building</a></li>
