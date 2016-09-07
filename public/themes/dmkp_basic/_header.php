6d663ec5188d33d593234b15ac70666cbd93c066
Ahmed Maawy <ultimateprogramer@gmail.com>
2016-09-07 08:35:07 +0300

    Change page title for `dmkp_basic` theme to Disaster Managment Knowledge Portal

--- public/themes/dmkp_basic/_header.php
+++ public/themes/dmkp_basic/_header.php
@@ -1,6 +1,6 @@
 <head>
     <meta charset="utf-8">
-    <title><?php echo isset($page_title) ? $page_title. ' : ' : ''; ?>Emergency Management System - World Vision</title>
+    <title><?php echo isset($page_title) ? $page_title. ' : ' : ''; ?>Disaster Management Knowledge Portal - World Vision</title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="description" content="">
     <meta name="author" content="">
